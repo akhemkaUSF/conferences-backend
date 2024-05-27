8d63510baea0e034cafecb68b2fb0fb105db5488
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-27 19:57:28 -0400

    edited MONGO_URL

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     //contrary to what I assumed, the Booking and Place Models aren't assigned, which kinda makes sense
     name: String,
     //email must be unique -- if it's a duplicate, it results in an error (which we actually don't have a handler for right now)
-    email: {type: String, unique:true},
+    email: {type: String},
     password: String,
 });
 
