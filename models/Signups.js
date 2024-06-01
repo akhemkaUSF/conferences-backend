5271323fe2a942920a39d74ef2fa5f42fb303864
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-06-01 00:03:23 -0400

    minor change to hopefully fix issue with conference signups display

--- models/Signups.js
+++ models/Signups.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const signupSchema = new mongoose.Schema({
   //ref: 'Place' means that the ObectID stored in the place variable must match an objectID in the place collection  
   conference: {type:mongoose.Schema.Types.ObjectId, required:true, ref:'Conference'},
-  user: {type:mongoose.Schema.Types.ObjectId, required:true},
+  user: {type:mongoose.Schema.Types.ObjectId, required:true, ref: 'User'},
   canDrive: Boolean,
   passengers: Number,
   delegateFeePaid: Boolean,
