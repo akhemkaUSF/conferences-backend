94c849598c7c895ff687b750edf1ae34314f7a52
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-06-02 12:41:21 -0400

    wrapping data around parentheses

--- index.js
+++ index.js
@@ -297,7 +297,7 @@ app.put('/user', async (req,res) => {
 app.delete('/user/:id', async (req,res) => {
   mongoose.connect(process.env.MONGO_URL);
   const {userID} = req.params;
-  const data = await Signup.find({user:userID});
+  const {data} = await Signup.find({user:userID});
   console.log(data);
   if (data!=null) {
     res.json(data);
