9f19c1d58d9615a6bfe0291303328f7c9e8efa0a
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-06-16 19:43:19 -0400

    test configuration of unit test

--- users.test.js
+++ users.test.js
@@ -1,10 +1,14 @@
 //users.test.js
 const request = require('supertest');
-import app from './index.js';
+//import app from './index.js';
 
-test('should return test ok', async () => {
+/*test('should return test ok', async () => {
   const response = await request(app).get('/test');
   console.log(response);
   expect(response.status).toBe(200);
   expect(response.body).toEqual("test ok");
+});*/
+
+test('test console.log', () => {
+    console.log("is this permission denied too");
 });
